d1cb651322113472fd6b291a0def327046f53e29
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2023-05-05 01:40:56 +0300

    feat: deploy 1.0

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,6 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://sharon:1234@cluster0.tu61v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//   'dbURL': 'mongodb://localhost:27017',
+  // 'dbURL': 'mongodb+srv://sharon:1234@cluster0.tu61v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  dbURL:
+    'mongodb+srv://Airbnb:Avior302!@cluster0.rpqqi7b.mongodb.net/?retryWrites=true&w=majority',
+  //   'dbURL': 'mongodb://localhost:27017',
 }
